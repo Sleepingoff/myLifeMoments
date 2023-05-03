7addf4cbff8c61c73f5a3f9305d7e113a10b577c
Sleepingoff <just_jh30@naver.com>
2023-05-03 12:31:33 +0900

    add weather API

---
+++ js/weather.js
@@ -0,0 +1,14 @@
+const API_key = "a37c09862afebcdbce4c5f450a8d4caf";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+    fetch(url).then(response => response.json()).then(date => console.log(date.name, date.weather[0].main));
+}
+function onGeoError(){
+    alert("Can't find you");
+}
+
+//위치정보 불러오기
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
