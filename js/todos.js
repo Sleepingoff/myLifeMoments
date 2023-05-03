07ba8e082081fecd7efe60d5861b1b9f4b095ff0
Sleepingoff <just_jh30@naver.com>
2023-05-03 10:20:03 +0900

    make todolist store obj

--- js/todos.js
+++ js/todos.js
@@ -9,16 +9,17 @@ function saveToDos(){
     localStorage.setItem(DOTO_KEY, JSON.stringify(toDos));
 }
 //할 일 추가하기
-function paintToDo(newToDo){
+function paintToDo(newToDoObj){
     //elements in list
     const li = document.createElement("li");
     const span = document.createElement("span");
     const delbtn = document.createElement("button");
     delbtn.addEventListener("click", deleteList);
+    li.id = newToDoObj.id;
     li.appendChild(span);
     li.appendChild(delbtn);
     toDoList.appendChild(li);
-    span.innerText = newToDo;
+    span.innerText = newToDoObj.text;
     delbtn.innerText = "x";
 }
 //할 일 입력받기
@@ -26,11 +27,14 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const toDoValue = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(toDoValue);
-    paintToDo(toDoValue);
+    //toDo를 array가 아닌 obj의 text로 추가하며, 개별로 아이디를 만들어 구별이 가능하도록 하기. 아이디는 랜덤한 숫자
+    const toDoObj = {text: toDoValue, id: Date.now()};
+    toDos.push(toDoObj);
+    paintToDo(toDoObj);
     saveToDos();
 }
 //할 일 삭제하기
+//먼저 obj의 아이디를 가져오도록 다시 설정해야함
 function deleteList(event){
     const target = event.target.parentElement;
     target.remove();
