4573ddc6a0849f41d5eb22b35cf06ac7b22b9669
Sleepingoff <just_jh30@naver.com>
2023-05-05 12:40:02 +0900

    add clock.padStart()

--- js/clock.js
+++ js/clock.js
@@ -6,12 +6,17 @@ $(document).ready(function(){
     function showDate(){
         let month = new Date().getMonth() + 1;
         let date = new Date().getDate();
+        month = month.toString().padStart(2, "0");
+        date = date.toString().padStart(2, "0");
         $(".clock .h1").text(month+" / "+date);
     }
     function showClock(){
         let hour = new Date().getHours();
         let min = new Date().getMinutes();
         let sec = new Date().getSeconds();
+        hour = hour.toString().padStart(2, "0");
+        min = min.toString().padStart(2, "0");
+        sec = sec.toString().padStart(2, "0");
         $(".clock .h3").text(hour+" : "+min+" : "+sec);
     }
 });
