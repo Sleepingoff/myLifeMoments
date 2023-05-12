d9e471c0b82c4c5b6e254260516bb9c06bd5f283
Sleepingoff <just_jh30@naver.com>
2023-05-12 12:32:34 +0900

    add script.html for scripts under footer

--- js/header.js
+++ js/header.js
@@ -4,40 +4,39 @@ const headerDown = "down";
 
 let startScrollValue = document.documentElement.scrollTop;
 let animate;
-function upHeader(){
-    header.classList.remove(headerDown);
-    header.classList.add(headerUp);
+function upHeader() {
+  header.classList.remove(headerDown);
+  header.classList.add(headerUp);
 }
-function downHeader(){
-    header.classList.remove(headerUp);
-    header.classList.add(headerDown);
-    timeUp();
+function downHeader() {
+  header.classList.remove(headerUp);
+  header.classList.add(headerDown);
 }
-function removeAll(){
-    header.classList.remove(headerUp);
-    header.classList.remove(headerDown);
-    clearTimeout(animate);
+function removeAll() {
+  header.classList.remove(headerUp);
+  header.classList.remove(headerDown);
+  clearTimeout(animate);
 }
-function timeUp(){
-    animate = setTimeout(upHeader,3000);
+function timeUp() {
+  animate = setTimeout(upHeader, 3000);
 }
 function scrollHeader() {
-    removeAll();
-    let currentScrollValue = document.documentElement.scrollTop;
-    let scrollValue = startScrollValue - currentScrollValue;
-    scrollValue < 0 ? downHeader() : upHeader();
-    if(currentScrollValue === 0) removeAll();
-    startScrollValue = currentScrollValue;
+  removeAll();
+  let currentScrollValue = document.documentElement.scrollTop;
+  let scrollValue = startScrollValue - currentScrollValue;
+  if (scrollValue < 0) {
+    downHeader();
+    timeUp();
+  } else {
+    upHeader();
+  }
+  if (currentScrollValue === 0) removeAll();
+  startScrollValue = currentScrollValue;
 }
 document.addEventListener("scroll", scrollHeader);
 
-header.addEventListener("mouseenter", function(){
-    if(header.classList.contains(headerUp)){
-        downHeader();
-    }
+header.addEventListener("mouseenter", function () {
+  downHeader();
+  clearTimeout(animate);
 });
-header.addEventListener("mouseleave", function(){
-    if(header.classList.contains(headerUp)){
-        scrollHeader();
-    }
-});
+header.addEventListener("mouseleave", scrollHeader);
