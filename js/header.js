5a8f436b098a667aacbffa3326789882648638dc
Sleepingoff <just_jh30@naver.com>
2023-05-12 10:45:11 +0900

    make header move!

--- js/header.js
+++ js/header.js
@@ -1,19 +1,45 @@
-window.onload = function(){
-    const header = document.querySelector("header");
-    console.log(header);
-    const headerUp = "up";
-    const headerDown = "down";
-    let startScrollValue = document.documentElement.scrollTop;
-    header.addEventListener("scroll", function(){
-        let currentScrollValue = document.documentElement.scrollTop;
-        let scrollValue = startScrollValue - currentScrollValue;
-        if(scrollValue > 0){
-            header.classList.add(headerUp);
-        }
-        if(scrollValue < 0){
-            header.classList.add(headerDown);
-        }
-        startScrollValue = currentScrollValue;
-    })
-    alert('ready');
+const header = document.querySelector("header");
+console.log(header);
+const headerUp = "up";
+const headerDown = "down";
+
+let startScrollValue = document.documentElement.scrollTop;
+function upHeader(){
+    header.classList.remove(headerDown);
+    header.classList.add(headerUp);
 }
+function downHeader(){
+    header.classList.remove(headerUp);
+    header.classList.add(headerDown);
+}
+function removeAll(){
+    header.classList.remove(headerUp);
+    header.classList.remove(headerDown);
+}
+function scrollHeader() {
+    let currentScrollValue = document.documentElement.scrollTop;
+    let scrollValue = startScrollValue - currentScrollValue;
+    /*if (scrollValue > 0) {
+        upHeader();
+    }
+    if (scrollValue < 0) {
+        downHeader();
+    }
+    if (currentScrollValue === 0){
+        removeAll();
+    }*/
+    currentScrollValue === 0 ? removeAll() : (scrollValue < 0 ? downHeader() : upHeader());
+    startScrollValue = currentScrollValue;
+}
+document.addEventListener("scroll", scrollHeader);
+
+header.addEventListener("mouseenter", function(){
+    if(header.classList.contains(headerUp)){
+        downHeader();
+    }
+});
+header.addEventListener("mouseleave", function(){
+    if(header.classList.contains(headerUp)){
+        scrollHeader();
+    }
+});
