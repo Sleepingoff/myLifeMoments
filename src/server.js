d835ec4d36e810ef156c92df29bf5bb261b7d45a
Sleepingoff <just_jh30@naver.com>
2023-05-20 13:27:00 +0900

    feat server.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import http from "http";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+app.use("/js", express.static(__dirname + "/js"));
+app.get("/", (_, res) => res.render("main"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`listening on http://localhost:3000`);
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000, handleListen);
